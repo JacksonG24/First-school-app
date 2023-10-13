ed565cd82bb39d00e912634a26978d3cb667a58b
JACKSONG24 <JRGAMMACK24@BLAKESCHOOL.ORGm>
2023-10-13 13:17:18 -0500

    Initial Commit

---
+++ First school app/First_school_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_school_appApp.swift
+//  First school app
+//
+//  Created by Jackson Gammack24 on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct First_school_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
